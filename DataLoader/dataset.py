a624468c3dd87b8bf9b8ede639fb17cc3b6657dd
Jiatao Xiang <jiatao.xiang@gmail.com>
2021-04-05 14:38:04 -0400

    correctly add bounding box to images

--- DataLoader/dataset.py
+++ DataLoader/dataset.py
@@ -4,6 +4,9 @@ import pathlib
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
+import cv2 as cv
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 class MaskDataset(Dataset):
     def __init__(self, input_dirs_path, target_dirs_path, transformer=ToTensor()):
@@ -30,16 +33,38 @@ class MaskDataset(Dataset):
         return inputImage, targetLabels
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     trainPath = pathlib.Path.cwd() / "dataset/train"
     dataset = MaskDataset(trainPath / "images", trainPath / "labels")
-    print(dataset[0])
+    image, labels = dataset[0]
+    # denormalize
+    mul = [1,2000,1363,2000,1363] #[constant, image_width, image_height, image_width, image_height]
+    finalStage = []
+    for x in labels:
+        c, xx, yy, w, h = x[0] * mul[0], x[1] * mul[1], x[2] * mul[2], x[3] * mul[3], x[4] * mul[4]
+        finalStage.append([c, xx, yy, w, h])
+    
+    fig = plt.figure()
+    # add axis at position rect
+    ax = fig.add_axes([0,0,1,1])
+
+    plt.imshow(image.permute(1, 2, 0))
+
+    for x in finalStage:
+        class_ = int(x[0])
+        xcenter, ycenter, width, height = x[1:]
+        xmin, ymin = xcenter - width//2, ycenter - width//2
+        xmax, ymax = xcenter + width//2, ycenter + height//2
+        if class_ == 0:
+            edgecolor = "r"
+            ax.annotate('MASK', xy=(xmax - 40, ymin + 20))
+        elif class_ == 1:
+            edgecolor = "b"
+            ax.annotate('NO MASK', xy=(xmax - 40, ymin + 20))
+        # adding bounding box
+        rect = patches.Rectangle((xmin,ymin), width, height, edgecolor = edgecolor, facecolor = 'none')
+        ax.add_patch(rect)
+    
+    plt.show()
+
+
