25a09451a7e990e398623b0e3d5c607ed6ac09bc
Jiatao Xiang <jiatao.xiang@gmail.com>
2021-04-08 17:21:11 -0400

    preprocess label so that it is suitable for training

---
+++ DataLoader/MaskDataset.py
@@ -0,0 +1,62 @@
+import os
+import torch
+import pathlib
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+import cv2 as cv
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+class MaskDataset(Dataset):
+    def __init__(self, input_dirs_path, target_dirs_path, S=7, B=2, C=2, transformer=ToTensor()):
+        self.input_dirs_path = input_dirs_path
+        self.target_dirs_path = target_dirs_path
+        self.input_dirs = os.listdir(input_dirs_path)
+        self.target_dirs = os.listdir(target_dirs_path)
+        self.transformer = transformer
+        self.S = S
+        self.B = B
+        self.C = C
+
+    def __len__(self):
+        return len(self.input_dirs)
+
+    def __getitem__(self, index):
+        inputImage = self.transformer(Image.open(self.input_dirs_path / self.input_dirs[index]))
+        targetFile = open(self.target_dirs_path / self.target_dirs[index])
+        targets = []
+        for line in targetFile:
+            targets.append(line.strip().split())
+        targetFile.close()
+        # convert string to int
+        targetLabels = []
+        for i in range(len(targets)):
+            target = list(map(float, targets[i]))
+            target[0] = int(target[0])
+            targetLabels.append(target)
+        # label matrix
+        label_matrix = torch.zeros((self.S, self.S, self.C + 5))
+        for box in targetLabels:
+            class_label, x, y, width, height = box
+            class_label = int(class_label)
+            # get label relative to each cell
+            i, j = int(self.S * y), int(self.S * x)
+            x_cell, y_cell = self.S * x - j, self.S * y - i
+            width_cell, height_cell = width * self.S, height * self.S
+            if label_matrix[i, j, self.C] == 0: # set to 1 if cell exist object
+                label_matrix[i, j, self.C] = 1
+                box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
+                label_matrix[i, j, self.C + 1 :] = box_coordinates
+                label_matrix[i, j, class_label] = 1
+        return inputImage, targetLabels, label_matrix
+
+
+if __name__ == "__main__":
+    trainPath = pathlib.Path.cwd() / "dataset/train"
+    dataset = MaskDataset(trainPath / "images", trainPath / "labels")
+    image, labels, label_matrix = dataset[0]
+    print(label_matrix)
+    
+
+
