5350b43006a7fbd7e06dbbdc96dcaf19396ee27f
Jiatao Xiang <jiatao.xiang@gmail.com>
2021-04-08 17:21:59 -0400

    template for system path setting

---
+++ Setup/setup.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.curdir, "Utils"))
+sys.path.insert(0, os.path.join(os.path.curdir, "DataLloader"))
+sys.path.insert(0, os.path.join(os.path.curdir, "Model"))
+sys.path.insert(0, os.path.join(os.path.curdir, "Test"))
+sys.path.insert(0, os.path.join(os.path.curdir, "Train"))
+
+from util import (
+    non_max_suppression,
+    mean_average_precision,
+    intersection_over_union,
+    cellboxes_to_boxes,
+    get_bboxes,
+    plot_image,
+    save_checkpoint,
+    load_checkpoint,
+)
+
+print(intersection_over_union)
