ebbe3b362ff825d45fc0363623c4ad54aa51bdd0
Jiatao Xiang <jiatao.xiang@gmail.com>
2021-04-09 14:29:37 -0400

    fix naming error

--- Setup/setup.py
+++ Setup/setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.join(os.path.curdir, "Utils"))
-sys.path.insert(0, os.path.join(os.path.curdir, "DataLloader"))
+sys.path.insert(0, os.path.join(os.path.curdir, "DataLoader"))
 sys.path.insert(0, os.path.join(os.path.curdir, "Model"))
 sys.path.insert(0, os.path.join(os.path.curdir, "Test"))
 sys.path.insert(0, os.path.join(os.path.curdir, "Train"))
